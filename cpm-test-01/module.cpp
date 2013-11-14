bfb5ccc384ce1535440ae656478499788bfe1c6a
James Hughes <jhughes@sci.utah.edu>
2013-11-13 23:06:47 -0700

    version 0.1

--- cpm-test-01/module.cpp
+++ cpm-test-01/module.cpp
@@ -5,7 +5,7 @@ namespace CPM_TEST_01_NS {
 
 std::string test01Function(std::string str)
 {
-  return str + "test_01_version_0";
+  return str + "test_01_version_0.1";
 }
 
 } // namespace CPM_TEST_01_NS 
