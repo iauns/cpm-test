42065a8246fddc7588998e6e4057d9cfec517495
James Hughes <jhughes@sci.utah.edu>
2013-11-13 23:31:58 -0700

    Version for master.

--- cpm-test-01/module.cpp
+++ cpm-test-01/module.cpp
@@ -5,10 +5,10 @@
 
 namespace CPM_TEST_01_NS {
 
-std::string test01Function(std::string str, int num)
+std::string test01Function(std::string str, int num, int num2)
 {
   std::stringstream ss;
-  ss << str << "test_01_v_0.3 - " << num;
+  ss << str << "test_01_v_MASTER - " << num << " - " << num2;
   return ss.str();
 }
 
--- cpm-test-01/module.hpp
+++ cpm-test-01/module.hpp
@@ -5,7 +5,7 @@
 
 namespace CPM_TEST_01_NS {
 
-std::string test01Function(std::string str, int num);
+std::string test01Function(std::string str, int num, int num2);
 
 } // namespace CPM_TEST_01_NS 
 
